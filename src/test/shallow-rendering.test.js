4771c13c61f275582c0c8d195c2882f912f36e39
Zhentian Wan <answer881215@gmail.com>
2017-02-06 14:52:40 +0200

    test partial rendering by using epxect-jsx

--- src/test/shallow-rendering.test.js
+++ src/test/shallow-rendering.test.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
+import expect from 'expect';
+import expectJSX from 'expect-jsx';
 
-const CoolComponent = ({greeting}) => (
-  <div>
-      <h1>Greeting</h1>
-      <div>{greeting}</div>
-  </div>
+expect.extend(expectJSX);
+
+
+const CoolComponent = ({ greeting }) => (
+    <div>
+        <h1>Greeting</h1>
+        <div>{greeting}</div>
+    </div>
 );
 
 describe('Cool Component', () => {
-   test('it should ....', () => {
-       const renderer = TestUtils.createRenderer();
-       renderer.render(<CoolComponent greeting="hello world"/>);
-       const output = renderer.getRenderOutput();
-       const expected = (
-           <div>
-               <h1>Greeting</h1>
-               <div>hello world</div>
-           </div>
-       );
-       expect(output).toEqual(expected);
-   })
+    test('it should render correctly.', () => {
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<CoolComponent greeting="hello world"/>);
+        const output = renderer.getRenderOutput();
+        const expected = (
+            <div>
+                <h1>Greeting</h1>
+                <div>hello world</div>
+            </div>
+        );
+        expect(output)
+            .toEqual(expected);
+    });
+
+    test('only piece of ui element', () => {
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<CoolComponent greeting="hello world"/>);
+        const result = renderer.getRenderOutput();
+        const expected = <div>hello world</div>;
+        expect(result).toIncludeJSX(expected);
+    });
 });
