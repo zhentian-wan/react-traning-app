e02aa1fa42a919cbc928f808a5f2ebc5e806cc6b
Zhentian Wan <answer881215@gmail.com>
2017-02-06 21:24:24 +0200

    conditional className with shallow rendering

--- src/test/box.test.js
+++ src/test/box.test.js
@@ -17,7 +17,7 @@ describe('box component', () => {
        expect(result).toEqual('div');
     });
 
-    test('box should be rendered correctly', () => {
+    test('box should be rendered correctly: red', () => {
        const renderer = TestUtils.createRenderer();
        renderer.render(<Box color="red" id="0"/>);
        const result = renderer.getRenderOutput().props.className.includes('red');
@@ -26,5 +26,16 @@ describe('box component', () => {
        const expected2 = false;
        expect(result).toEqual(expected);
        expect(result2).toEqual(expected2);
+    });
+
+    test('box should be rendered correctly: green', () => {
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<Box color="green" id="0"/>);
+        const result = renderer.getRenderOutput().props.className.includes('green');
+        const result2 = renderer.getRenderOutput().props.className.includes('red');
+        const expected = true;
+        const expected2 = false;
+        expect(result).toEqual(expected);
+        expect(result2).toEqual(expected2);
     });
 });
