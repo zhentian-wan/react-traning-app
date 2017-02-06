0f78e7ce546c98a3f12cb6759288d8807ed879fa
Zhentian Wan <answer881215@gmail.com>
2017-02-06 21:21:28 +0200

    className with shallow rendering

--- src/test/box.test.js
+++ src/test/box.test.js
@@ -16,4 +16,15 @@ describe('box component', () => {
        const result = renderer.getRenderOutput().type;
        expect(result).toEqual('div');
     });
+
+    test('box should be rendered correctly', () => {
+       const renderer = TestUtils.createRenderer();
+       renderer.render(<Box color="red" id="0"/>);
+       const result = renderer.getRenderOutput().props.className.includes('red');
+       const result2 = renderer.getRenderOutput().props.className.includes('green');
+       const expected = true;
+       const expected2 = false;
+       expect(result).toEqual(expected);
+       expect(result2).toEqual(expected2);
+    });
 });
