0244c274614881ed448c3454c08df4847f3c2395
Zhentian Wan <answer881215@gmail.com>
2017-02-07 12:26:09 +0200

    reuse function

--- src/test/box.test.js
+++ src/test/box.test.js
@@ -17,25 +17,21 @@ describe('box component', () => {
        expect(result).toEqual('div');
     });
 
-    test('box should be rendered correctly: red', () => {
-       const renderer = TestUtils.createRenderer();
-       renderer.render(<Box color="red" id="0"/>);
-       const result = renderer.getRenderOutput().props.className.includes('red');
-       const result2 = renderer.getRenderOutput().props.className.includes('green');
-       const expected = true;
-       const expected2 = false;
-       expect(result).toEqual(expected);
-       expect(result2).toEqual(expected2);
+    describe('color rendering tests', () => {
+        function renderingBoxThenCheckColor(color) {
+            const renderer = TestUtils.createRenderer();
+            renderer.render(<Box color={color} id="0"/>);
+            return renderer.getRenderOutput().props.className.includes(color);
+        }
+
+        test('box should be rendered correctly: red', () => {
+            expect(renderingBoxThenCheckColor('red')).toEqual(true);
+        });
+
+        test('box should be rendered correctly: green', () => {
+            expect(renderingBoxThenCheckColor('green')).toEqual(true);
+        });
     });
 
-    test('box should be rendered correctly: green', () => {
-        const renderer = TestUtils.createRenderer();
-        renderer.render(<Box color="green" id="0"/>);
-        const result = renderer.getRenderOutput().props.className.includes('green');
-        const result2 = renderer.getRenderOutput().props.className.includes('red');
-        const expected = true;
-        const expected2 = false;
-        expect(result).toEqual(expected);
-        expect(result2).toEqual(expected2);
-    });
+
 });
