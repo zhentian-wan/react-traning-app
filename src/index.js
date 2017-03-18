1b8f8c32a343fa7e17ba1c1c2d95d5f39bfed420
Zhentian Wan <answer881215@gmail.com>
2017-03-18 19:23:25 +0100

    Link

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 // import {Router} from './router';
 import {
     BrowserRouter as Router,
-    Route
+    Route, Link
 } from 'react-router-dom';
 
 // provider required by material-ui
@@ -15,16 +15,27 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import App from './components/App';
 import About from './components/About';
+import Contact from './components/Contact';
 
 injectTapEventPlugin();
+
+const Nav = () => (
+  <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link replace to={{pathname: '/contact'}}>Contact</Link>
+  </nav>
+);
 
 ReactDOM.render(
     <MuiThemeProvider>
         <Router>
             <div>
+                <Nav></Nav>
                 <Route exact path="/" component={App}></Route>
                 <Route exact path="/about" component={About}></Route>
-                <Route
+                <Route exact path="/contact" component={Contact}></Route>
+                {/*<Route
                     strict
                     path="/about/"
                     render={() => <h2>About render</h2>}></Route>
@@ -33,7 +44,7 @@ ReactDOM.render(
                     children={({match}) => {
                         console.log("match:", match)
                         return match && <h2>demo</h2>
-                    }}></Route>
+                    }}></Route>*/}
             </div>
         </Router>
     </MuiThemeProvider>, document.getElementById('root'));
